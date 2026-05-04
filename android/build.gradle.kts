fce7ebb459c417362009870be7706075129a2ced
Thierry <thierryud2476@gmail.com>
2026-05-04 18:12:49 +0700

    it workss lets gooo

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,10 +24,8 @@ tasks.register<Delete>("clean") {
 }
 
 plugins {
-  // ...
 
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.4.4" apply false
+  id("com.google.gms.google-services") version "4.3.10" apply false
 
 }
 
